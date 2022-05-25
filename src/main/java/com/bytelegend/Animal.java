1c97aecf35c46decb3e50b4ddc5d69578924025b
zhicheng-ning <2516085027@qq.com>
2022-05-25 13:50:04 +0000

    Update at 2022-05-25T13:50:04.019672Z

--- src/main/java/com/bytelegend/Animal.java
+++ src/main/java/com/bytelegend/Animal.java
@@ -1,3 +1,8 @@
 package com.bytelegend;
 
-public class Animal {}
+public class Animal {
+    private final String name;
+    public void sayMyName() {
+        System.out.println("my name is: " + name);
+    }
+}
