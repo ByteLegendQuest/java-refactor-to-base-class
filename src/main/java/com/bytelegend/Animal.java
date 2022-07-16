6d265e87b86be76126b0605c739444ce1234d8d6
zerofancy <ntutn.top@gmail.com>
2022-07-17 04:52:03 +0800

    answer

--- src/main/java/com/bytelegend/Animal.java
+++ src/main/java/com/bytelegend/Animal.java
@@ -1,3 +1,9 @@
 package com.bytelegend;
 
-public class Animal {}
+public class Animal {
+    protected String name;
+
+    public void sayMyName() {
+        System.out.println("my name is: " + name);
+    }
+}
