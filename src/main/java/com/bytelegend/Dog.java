dacf47e4b39e2aee8d578f083ce5cff2ce54a48e
pipiloveslife <Pain_Zzz@163.com>
2022-10-17 08:15:53 +0000

    Update at 2022-10-17T08:15:53.747142Z

--- src/main/java/com/bytelegend/Dog.java
+++ src/main/java/com/bytelegend/Dog.java
@@ -1,14 +1,9 @@
 package com.bytelegend;
 
 public class Dog {
-    private final String name;
 
     public Dog(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("my name is: " + name);
+        super(name);
     }
 
     public void woof() {
