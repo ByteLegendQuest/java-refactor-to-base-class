fcc8c11272e63dc21cdb2d11a6df127bd8718d04
Anlyui <1391469813@qq.com>
2022-10-18 09:28:52 +0000

    Update at 2022-10-18T09:28:52.873450Z

--- src/main/java/com/bytelegend/Sheep.java
+++ src/main/java/com/bytelegend/Sheep.java
@@ -1,15 +1,16 @@
 package com.bytelegend;
 
-public class Sheep {
-    private final String name;
+public class Sheep extends Animal{
+//    private final String name;
 
     public Sheep(String name) {
-        this.name = name;
+//        this.name = name;
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("my name is: " + name);
-    }
+//    public void sayMyName() {
+//        System.out.println("my name is: " + name);
+//    }
 
     public void meh() {
         System.out.print("meh, ");
