23bb1a4160f517dd5ccafc43878ef90fc90ca789
keranbingaa <binghong@mail.ustc.edu.cn>
2022-12-19 15:36:57 +0000

    Update at 2022-12-19T15:36:57.208550Z

--- src/main/java/com/bytelegend/Dog.java
+++ src/main/java/com/bytelegend/Dog.java
@@ -1,14 +1,8 @@
 package com.bytelegend;
 
-public class Dog {
-    private final String name;
-
+public class Dog extends Animal {
     public Dog(String name) {
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("my name is: " + name);
     }
 
     public void woof() {
