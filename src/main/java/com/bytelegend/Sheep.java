92e4e785e03409433f9f37aa73a111917bce3b47
williamgl <ligan@oregonstate.edu>
2023-07-08 20:35:23 -0700

    Update

--- src/main/java/com/bytelegend/Sheep.java
+++ src/main/java/com/bytelegend/Sheep.java
@@ -1,16 +1,7 @@
 package com.bytelegend;
 
-public class Sheep {
-    private final String name;
-
-    public Sheep(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("my name is: " + name);
-    }
-
+public class Sheep extends Animal{
+    public Sheep(String name) {super(name);}
     public void meh() {
         System.out.print("meh, ");
         sayMyName();
